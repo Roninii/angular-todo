23f2c78bc79fdccaff2207878f59086bb2d03fd9
Ronnie Villarini <Villarini91@gmail.com>
2019-05-01 16:29:43 -0500

    initial commit

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Todo } from "../models/Todo";
+import { Observable } from "rxjs";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Content-Type": "application/json"
+  })
+};
+
+@Injectable({
+  providedIn: "root"
+})
+export class TodoService {
+  todosUrl = "https://jsonplaceholder.typicode.com/todos";
+  todosLimit = `?_limit=5`;
+
+  constructor(private http: HttpClient) {}
+
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+  }
+
+  toggleCompleted(todo: Todo): Observable<any> {
+    return this.http.put(`${this.todosUrl}/${todo.id}`, todo, httpOptions);
+  }
+}
